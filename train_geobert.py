8a94ff659d435add97bf4c95d269a7d1f2e30102
Hagen <hagen.hoferichter@gmail.com>
2021-11-23 16:33:42 +0100

    add exception logging

--- train_geobert.py
+++ train_geobert.py
@@ -60,53 +60,60 @@ for epoch in range(0,NEPOCHS):
     logging.info(f"Starting training.")
     model.train()
     for iteration, files in enumerate(train_loader):
-        optim.zero_grad()
-        batch = files['enc_dict.pyd']
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=labels, output_hidden_states=True)
-        logits = outputs.get('logits')
-        loss_fct = nn.HuberLoss()
-        train_loss = loss_fct(logits, labels)
-        train_loss.backward()
-        optim.step()
-        # Logging
-        train_loss_float = float(train_loss) 
-        train_losses.append(train_loss_float)
-        writer.add_scalar(LOGSTR + "-train", train_loss_float, iteration)
-        logging.info(f"E:{epoch:3d}, I:{iteration:8d} TRAIN: {train_loss_float:10.3f}")
-        del input_ids, attention_mask, labels, logits, train_loss
-        
+        try:
+            optim.zero_grad()
+            batch = files['enc_dict.pyd']
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels, output_hidden_states=True)
+            logits = outputs.get('logits')
+            loss_fct = nn.HuberLoss()
+            train_loss = loss_fct(logits, labels)
+            train_loss.backward()
+            optim.step()
+            # Logging
+            train_loss_float = float(train_loss) 
+            train_losses.append(train_loss_float)
+            writer.add_scalar(LOGSTR + "-train", train_loss_float, iteration)
+            logging.info(f"E:{epoch:3d}, I:{iteration:8d} TRAIN: {train_loss_float:10.3f}")
+            del input_ids, attention_mask, labels, logits, train_loss
+        except Exception as e:
+            logging.exception(e)
+
         
     # Eval in epoch
     logging.info(f"Starting evaluation.")
     model.eval()
     with torch.no_grad():
         for iteration, files in enumerate(test_loader):
-            val_batch = files['enc_dict.pyd']
-            val_input_ids = val_batch['input_ids'].to(device)
-            val_attention_mask = val_batch['attention_mask'].to(device)
-            val_labels = val_batch['labels'].to(device)
-            val_outputs = model(
-                val_input_ids, 
-                attention_mask=val_attention_mask, 
-                labels=val_labels, 
-                output_hidden_states=True)
-            val_logits = val_outputs.get('logits')
-            val_loss = loss_fct(val_logits, val_labels)
-            # Logging
-            val_loss_float = float(val_loss)
-            val_losses.append(val_loss_float)
-            writer.add_scalar(LOGSTR + "-test", val_loss_float, iteration)
-            logging.info(f"E:{epoch:3d}, I:{iteration:8d} TEST: {train_loss_float:10.3f}")        
-            del val_input_ids, val_attention_mask, val_labels, val_logits, val_loss
-                
+            try:
+                val_batch = files['enc_dict.pyd']
+                val_input_ids = val_batch['input_ids'].to(device)
+                val_attention_mask = val_batch['attention_mask'].to(device)
+                val_labels = val_batch['labels'].to(device)
+                val_outputs = model(
+                    val_input_ids, 
+                    attention_mask=val_attention_mask, 
+                    labels=val_labels, 
+                    output_hidden_states=True)
+                val_logits = val_outputs.get('logits')
+                val_loss = loss_fct(val_logits, val_labels)
+                # Logging
+                val_loss_float = float(val_loss)
+                val_losses.append(val_loss_float)
+                writer.add_scalar(LOGSTR + "-test", val_loss_float, iteration)
+                logging.info(f"E:{epoch:3d}, I:{iteration:8d} TEST: {train_loss_float:10.3f}")        
+                del val_input_ids, val_attention_mask, val_labels, val_logits, val_loss
+            except Exception as e:
+                logging.exception(e)
+
 
     avg_train_loss = np.mean(train_losses)
     avg_val_loss = np.mean(val_losses)
     writer.add_scalar(LOGSTR + "-train_epoch", avg_train_loss, epoch)
     writer.add_scalar(LOGSTR + "-test_epoch", avg_val_loss, epoch)
+    logging.info(f"E:{epoch:3d}, TRAIN: {avg_train_loss:10.3f}, TEST: {avg_val_loss:10.3f}")
     early_stopping(val_loss=avg_val_loss, model=model)
         
     if early_stopping.early_stop:
