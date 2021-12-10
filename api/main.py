b1ed2ef937c6270d9dfbfec589a6b09cfc732d63
Hagen <hagen.hoferichter@gmail.com>
2021-12-10 16:34:13 +0100

    cleaned api

--- api/main.py
+++ api/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI, Depends
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi import Security, FastAPI, HTTPException
-from fastapi.security.api_key import APIKeyQuery, APIKeyHeader, APIKey
+from fastapi import Security, HTTPException
+from fastapi.security.api_key import APIKeyHeader, APIKey
 from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse
 
 from transformers import DistilBertTokenizerFast
 
 from model import GeoModel
-from model import Coordinate, Meta, InputMetrics, PointResponse, GeoRequest
+from model import Coordinate, InputMetrics, PointResponse, GeoRequest
 from model import API_DESCRIPTION_STR
-from model import MAX_SEQ_LENGTH, BASE_MODEL, TOKEN_MODEL, API_VERSION, MODEL_VERSION
+from model import MAX_SEQ_LENGTH, BASE_MODEL, TOKEN_MODEL, API_VERSION
 
 geomodel = GeoModel(
     model_string=BASE_MODEL,
@@ -22,63 +21,57 @@ API_KEYS = [
     "geo",
 ]
 API_KEY_NAME = "access_token"
-
-api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 app = FastAPI(
-    title = "GEOFYR",
+    title="GEOFYR",
     version=API_VERSION,
     description=API_DESCRIPTION_STR
 )
 
 
-async def get_api_key(
-    api_key_query: str = Security(api_key_query),
-    api_key_header: str = Security(api_key_header),
-):
-
-    if api_key_query in API_KEYS:
-        return api_key_query
-    elif api_key_header in API_KEYS:
+async def validate_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header in API_KEYS:
         return api_key_header
     else:
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN,
             detail="Could not validate credentials"
         )
-        
 
-@app.get("/")
-async def root():
-    return {"msg": "Welcome to GEOFYR. Visit /docs for documentation <3"}
-    
 
-@app.post("/inference/point", response_model = PointResponse)
-def inference_point(request: GeoRequest, api_key: APIKey = Depends(get_api_key)):
+@app.get("/", include_in_schema=False)
+def docs_redirect():
+    return RedirectResponse("/docs")
+
+
+@app.post("/inference/point", response_model=PointResponse)
+def inference_point(request: GeoRequest,
+                    api_key: APIKey = Depends(validate_api_key)
+                    ):
     '''
     Infers the geographic location of a given text as a single point.
     For security reasons this endpoint only accepts the text as encrypted SSL payload. 
     '''
     # Split text to tokens
     split_text = request.text.split(" ")
-    
+
     # Slice tokens and calc token metrics
     input_tokens = len(split_text)
     inference_tokens = min(input_tokens, MAX_SEQ_LENGTH)
     inference_text = str(" ").join(split_text[:inference_tokens])
     unused_tokens = len(split_text[inference_tokens:])
-    
+
     # Inference
     geomodel.forward(inference_text)
     point_coordinate = geomodel.predict_point()
-    
+
     # Compose respond
     coordinate = Coordinate(lat=point_coordinate[0], lon=point_coordinate[1])
     input_metrics = InputMetrics(
-        input_tokens = input_tokens,
-        inference_tokens = inference_tokens,
-        unused_tokens = unused_tokens
+        input_tokens=input_tokens,
+        inference_tokens=inference_tokens,
+        unused_tokens=unused_tokens
     )
     return PointResponse(coordinate=coordinate, input_metrics=input_metrics)
 
--- api/model.py
+++ api/model.py
@@ -8,6 +8,7 @@ API_VERSION = '1.0'
 MODEL_VERSION = '1.0'
 MAX_SEQ_LENGTH = 200
 
+
 class GeoModel():
     def __init__(self, model_string, tokenizer_string, tokenizer_class, max_seq_length):
         self.model_string = model_string
@@ -47,38 +48,41 @@ class Coordinate(BaseModel):
                        example=52.5510,
                        title="Latitude",
                        description="Latitude of the predicted location."
-                      )
+                       )
     lon: float = Field(...,
                        example=13.3304,
                        title="Longitude",
                        description="Longitude of the predicted location."
-                      )
+                       )
+
 
 class Meta(BaseModel):
     api_version: str = Field(...,
-                        example='1.0',
-                        description="Version number of the API."
-                        )
+                             example='1.0',
+                             description="Version number of the API."
+                             )
     model_version: str = Field(...,
-                        example='1.0',
-                        description="Version number of the GEOFYR Neural Network."
-                        )
-    
+                               example='1.0',
+                               description="Version number of the GEOFYR Neural Network."
+                               )
+
+
 class InputMetrics(BaseModel):
     input_tokens: int = Field(...,
-                        example=300,
-                        description="The number of input tokens which where detected."
-                        )
+                              example=300,
+                              description="The number of input tokens which where detected."
+                              )
     inference_tokens: int = Field(...,
-                        example=200,
-                        description="The number of input tokens which where used for inference."
-                        )
+                                  example=200,
+                                  description="The number of input tokens which where used for inference."
+                                  )
     unused_tokens: int = Field(...,
-                        example=100,
-                        description=f"The number of input tokens which where ignored for inference.\
-                        Only {MAX_SEQ_LENGTH} tokens are allowed for inference. \
-                        Split your text beforehand into chunks of {MAX_SEQ_LENGTH} tokens for inference of larger texts."
-                        )
+                               example=100,
+                               description=f"The number of input tokens which where ignored for inference.\
+                               Only {MAX_SEQ_LENGTH} tokens are allowed for inference. \
+                               Split your text beforehand into chunks of {MAX_SEQ_LENGTH} tokens for inference of larger texts."
+                               )
+
 
 class PointResponse(BaseModel):
     meta: Meta = Meta(
@@ -86,7 +90,8 @@ class PointResponse(BaseModel):
         model_version=MODEL_VERSION)
     coordinate: Coordinate
     input_metrics: InputMetrics
-    
+
+
 class GeoRequest(BaseModel):
     text: str = Field(...,
                       max_tokens=MAX_SEQ_LENGTH,
@@ -94,7 +99,8 @@ class GeoRequest(BaseModel):
                       title="Text",
                       description="Text to infer geograpic location from. Maximum 200 tokens. A token is everything between two whitespaces. Make sure the string is web safe and all characters are escpaed properly."
                       )
-    
+
+
 API_DESCRIPTION_STR = """
     GEOFYR infers the geographic location of every kind of text.
     GEOFYR is based on state-of-the-art Natural Language Processing and smartly infers the to location of the content of a text based on its meaning and semamntic. GEOFYR is able to process Literature, News, Historical text and everything else. If there is any clue where the action of a text occurs, GEOFYR will find it.
